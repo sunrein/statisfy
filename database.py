87bb1ccfd62da8049eec4305fa9a14c5b797be38
sunrein <sabrina.unrein@gmail.com>
2017-02-15 11:39:01 -0700

    get db working, change structure a bit

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
-from flask.ext.sqlalchemy import SQLAlchemy()
-
-db = SQLAlchemy()
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[STATISFY]'
+db = SQLAlchemy(app)
