eafb2b4c314f4a7327fa5a2563d2462230e2ad8b
thejessleigh <j.l.unrein@gmail.com>
2017-02-19 13:37:24 -0600

    formatting and imports

--- statisfy/tests/test_books.py
+++ statisfy/tests/test_books.py
@@ -1,20 +1,22 @@
 import unittest
 
-from books.models import Book
+from statisfy.books.models import Book
+from database import db
+
 
 class BookTestCase(unittest.TestCase):
-	def setUp():
-		book = Book(
-			title="A Book",
-			author="Someone Something",
-			length_category="NOVEL",
-			age_group="ADULT"
-		)
-		db.session.add(book)
+    def setUp(self):
+        self.book = Book(
+            title="A Book",
+            author="Someone Something",
+            length_category="NOVEL",
+            age_group="ADULT"
+        )
+        db.session.add(self.book)
 
-	def test_book_creation(self):
-		self.assertIsNone(book.id)
-		db.session.commit(book)
-		self.assertIsInstance(book, Book)
-		self.assertIsNotNone(book.id)
-		self.assertIsInstance(book.id, int)
+    def test_book_creation(self):
+        self.assertIsNone(self.book.id)
+        db.session.commit(self.book)
+        self.assertIsInstance(self.book, Book)
+        self.assertIsNotNone(self.book.id)
+        self.assertIsInstance(self.book.id, int)
