7ace794c805fd8efeaebf147f02096e14795c7dc
sunrein <sabrina.unrein@gmail.com>
2017-03-02 17:45:38 -0700

    non-commital form exploration

---
+++ statisfy/templates/_create_acquisition.py
@@ -0,0 +1,9 @@
+{% extends "base.html" %}
+{% block content %}
+<h1>New Acquisition</h1>
+  <form action="" method="post" name="login">
+      <p>{{ form.remember_me }} Remember Me</p>
+      <p><input type="submit" value="Sign In"></p>
+  </form>
+
+{% endblock %}
